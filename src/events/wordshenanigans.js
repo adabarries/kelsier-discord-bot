0069562d31214b650aaaafd11ea60a30fc478bba
Ada <adabarries@gmail.com>
2024-03-02 22:51:59 -0800

    it works.

--- src/events/wordshenanigans.js
+++ src/events/wordshenanigans.js
@@ -1,63 +1,116 @@
-import { Client } from "discord.js";
+import { Events } from "discord.js";
 
-// to do: logic for determining if a certain phrase (two words) is 6 + 6 OR 4 + 6 and respond accordingly
-// example event 'something something fire emblem'
-// example response: 'Fire Emblem is a canon-compliant homestuck kid name.'
-
-// use includes() probably but you would need to find two substrings in a specific order....regex?
-
-
-const matchName = (msg) => {
-    const kidMatches = msg.match(/(\b[A-Z]{4}\s[A-Z]{6,7})/gi);
-    const canonKidNames = [ 
-        'john egbert', 
-        'jane crocker', 
-        'dave strider', 
-        'dirk strider',
-        'jade harley',
-        'rose lalonde',
-        'jake english',
-        'roxy lalonde'
-    ];
+// const matchName = (msg) => {
+//     const kidMatches = msg.match(/(\b[A-Z]{4}\s[A-Z]{6,7})/gi);
+//     const canonKidNames = [ 
+//         'john egbert', 
+//         'jane crocker', 
+//         'dave strider', 
+//         'dirk strider',
+//         'jade harley',
+//         'rose lalonde',
+//         'jake english',
+//         'roxy lalonde'
+//     ];
     
-    const trollMatches = msg.match(/(\b[A-Z]{6}\s[A-Z]{6})/gi);
-    const canonTrollNames = [
-        'aradia megido',
-        'tavros nitram',
-        'sollux captor',
-        'karkat vantas',
-        'nepeta leijon',
-        'kanaya maryam',
-        'terezi pyrope',
-        'vriska serket',
-        'equius zahhak',
-        'gamzee makara',
-        'eridan ampora',
-        'feferi peixes',
-        'damara megido',
-        'rufioh nitram',
-        'mituna captor',
-        'kankri vantas',
-        'meulin leijon',
-        'porrim maryam',
-        'latula pyrope',
-        'aranea serket',
-        'horuss zahhak', 
-        'kurloz makara',
-        'cronus ampora',
-        'meenah peixes'
-    ]; // wow. consider housing these elsewhere.
+//     const trollMatches = msg.match(/(\b[A-Z]{6}\s[A-Z]{6})/gi);
+//     const canonTrollNames = [
+//         'aradia megido',
+//         'tavros nitram',
+//         'sollux captor',
+//         'karkat vantas',
+//         'nepeta leijon',
+//         'kanaya maryam',
+//         'terezi pyrope',
+//         'vriska serket',
+//         'equius zahhak',
+//         'gamzee makara',
+//         'eridan ampora',
+//         'feferi peixes',
+//         'damara megido',
+//         'rufioh nitram',
+//         'mituna captor',
+//         'kankri vantas',
+//         'meulin leijon',
+//         'porrim maryam',
+//         'latula pyrope',
+//         'aranea serket',
+//         'horuss zahhak', 
+//         'kurloz makara',
+//         'cronus ampora',
+//         'meenah peixes'
+//     ]; // wow. consider housing these elsewhere.
 
-    if (kidMatches) {
-        if (canonKidNames.indexOf(kidMatches[0].toLowerCase()) === -1) {
-            console.log(`${kidMatches[0]} is a valid homestuck kid name.`);
+//     if (kidMatches) {
+//         if (canonKidNames.indexOf(kidMatches[0]) === -1) {
+//             console.log(`${kidMatches[0]} is a valid homestuck kid name.`);
+//             message.channel.send(`${kidMatches[0]} is a valid homestuck kid name.`);
+//         }
+//     } else if (trollMatches) {
+//         if (canonTrollNames.indexOf(trollMatches[0]) === -1) {
+//             console.log(`${trollMatches[0]} is a valid homestuck troll name.`);
+//             message.channel.send(`${trollMatches[0]} is a valid homestuck troll name.`);
+//         }
+//     } else return;
+// };
+
+export default {
+    name: Events.MessageCreate,
+    once: false,
+    async execute(message) {
+        const msg = message.content.toLowerCase();
+        if (message.author.bot) return;
+
+        // ----- homestuck name goof. ----- //
+        const kidMatches = message.match(/(\b[A-Z]{4}\s[A-Z]{6,7})/gi);
+        const trollMatches = message.match(/(\b[A-Z]{6}\s[A-Z]{6})/gi);
+
+        const canonKidNames = [ 
+            'john egbert', 
+            'jane crocker', 
+            'dave strider', 
+            'dirk strider',
+            'jade harley',
+            'rose lalonde',
+            'jake english',
+            'roxy lalonde'
+        ];
+
+        const canonTrollNames = [
+            'aradia megido',
+            'tavros nitram',
+            'sollux captor',
+            'karkat vantas',
+            'nepeta leijon',
+            'kanaya maryam',
+            'terezi pyrope',
+            'vriska serket',
+            'equius zahhak',
+            'gamzee makara',
+            'eridan ampora',
+            'feferi peixes',
+            'damara megido',
+            'rufioh nitram',
+            'mituna captor',
+            'kankri vantas',
+            'meulin leijon',
+            'porrim maryam',
+            'latula pyrope',
+            'aranea serket',
+            'horuss zahhak', 
+            'kurloz makara',
+            'cronus ampora',
+            'meenah peixes'
+        ]; // wow. consider housing these elsewhere.
+
+        if (kidMatches) {
+            if (canonKidNames.indexOf(kidMatches[0]) === -1) {
+                message.channel.send(`${kidMatches[0]} is a valid homestuck kid name.`);
+            }
+        } else if (trollMatches) {
+            if (canonTrollNames.indexOf(trollMatches[0]) === -1) {
+                message.channel.send(`${trollMatches[0]} is a valid homestuck troll name.`);
+            }
         }
-    } else if (trollMatches) {
-        if (canonTrollNames.indexOf(trollMatches[0].toLowerCase()) === -1) {
-            console.log(`${trollMatches[0]} is a valid homestuck troll name.`);
-            return trollMatches[0]; // convert this to a message event later
-        }
-    } else return;
+    },
 };
-
-const name = 'messageCreate';
